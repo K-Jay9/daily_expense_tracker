2e6bb3fb6fe58fe3084577866fc9ca2fffa69c59
kjempire9 <kjempirecode@gmail.com>
2021-03-04 17:50:07 +0300

    Added a gitignore and initialised tkinter

---
+++ tracker.py
@@ -0,0 +1,18 @@
+from tkinter import Tk
+
+
+# Code Constants and variables
+
+WIDHT = 0
+HEIGHT = 0
+NAME = 'Daily Expense Tracker'
+
+
+# Initialising the window
+window = Tk()
+
+# Adding the title to the window
+window.title(NAME)
+
+# Starting the mainloop
+window.mainloop()
